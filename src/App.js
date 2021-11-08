f085e382daa0caa19cbf4539d6827f9fb6be4efe
Manuel <m_guzman74@yahoo.com>
2021-11-08 00:06:33 -0600

    added LoginPage with conditional rendering based on user login status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, { useState } from "react";
 import ReactRouter from "./ReactRoutes";
 import "./App.css";
 
+import AuthContext from "./auth/context";
+import LoginPage from "./Pages/LoginPage";
+
 function App() {
+  const [user, setUser] = useState();
+
   return (
-    <div className="App">
-      <ReactRouter />
-    </div>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <div className="App">{user ? <ReactRouter /> : <LoginPage />}</div>
+    </AuthContext.Provider>
   );
 }
 
---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <div> this is the login screen</div>;
+};
+
+export default LoginPage;
