52def1516a8d01626209fa183ed75efd97d9ef55
Manuel <m_guzman74@yahoo.com>
2021-11-08 01:01:16 -0600

    css added to LoginPage

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+import styles from "../cssModules/LoginPage.module.css";
 
 const LoginPage = () => {
-  return <div> this is the login screen</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitFunction = (data) => console.log(data);
+
+  return (
+    <div className={styles.mainWrapper}>
+      <div className={styles.appTitleWrapper}>
+        <h1>Tally</h1>
+      </div>
+      <form
+        className={styles.formWrapper}
+        onSubmit={handleSubmit(submitFunction)}
+      >
+        <div className={styles.inputWrapper}>
+          <label htmlFor="username">Username</label>
+          <input
+            className={styles.loginInput}
+            type="text"
+            placeholder="Username"
+            name="username"
+            {...register("username", { required: true })}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="password">Password</label>
+          <input
+            className={styles.loginInput}
+            type="password"
+            placeholder="Password"
+            name="password"
+            {...register("password", { required: true })}
+          />
+        </div>
+        <input className={styles.loginSubmit} type="submit" value="Login" />
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
