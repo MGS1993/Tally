2afaa51f8c7e8e30c3fba29c72a77bd99c57f9e5
Manuel <m_guzman74@yahoo.com>
2021-11-22 01:26:41 -0600

    temp logout btn

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
+import AuthContext from "../auth/context";
 import TallyDisplay from "../components/TallyDisplay";
 
 const Home = ({ data }) => {
+  const authContext = useContext(AuthContext);
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    authContext.setUser(null);
+  };
   return (
     <div style={mainWrapper}>
       <TallyDisplay data={data} />
+      <button style={tempButton} onClick={() => logout()}>
+        test
+      </button>
     </div>
   );
 };
@@ -13,6 +23,12 @@ const Home = ({ data }) => {
 const mainWrapper = {
   display: "flex",
   flexDirection: "column",
+};
+
+const tempButton = {
+  height: "25px",
+  width: "30vw",
+  margin: "25px",
 };
 
 export default Home;
