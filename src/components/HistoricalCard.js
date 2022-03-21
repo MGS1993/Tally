e639156d60aec7a4228f59d295f21835213c2d06
Manuel <m_guzman74@yahoo.com>
2022-03-20 21:32:57 -0500

    added expandable container to historicalCard

--- src/components/HistoricalCard.js
+++ src/components/HistoricalCard.js
@@ -1,11 +1,37 @@
-import { StyledCard } from "./styles/HistoricalCard.styled";
+import { useState } from "react";
+import {
+  StyledCard,
+  MinimizedContainer,
+  ExpandableContainer,
+} from "./styles/HistoricalCard.styled";
 import { format, parseISO } from "date-fns";
-const HistoricalCard = ({ cost, date, ownerName, accentColor }) => {
+import Icon from "./Icon";
+import { BsChevronCompactDown } from "react-icons/bs";
+
+const HistoricalCard = ({
+  cost,
+  date,
+  ownerName,
+  accentColor,
+  title,
+  initialCost,
+}) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <StyledCard borderColor={accentColor}>
-      <div>{ownerName}</div>
-      <div>{cost}</div>
-      <div>{format(parseISO(date), "MM-d-yy")}</div>
+      <MinimizedContainer>
+        <div>{ownerName}</div>
+        <div>{cost}</div>
+        <div>{format(parseISO(date), "MM-d-yy")}</div>
+      </MinimizedContainer>
+      <ExpandableContainer expanded={expanded}>
+        <div> {title}</div>
+        <div>TotalExpense: {initialCost} </div>
+      </ExpandableContainer>
+      <div onClick={() => setExpanded(!expanded)}>
+        <Icon IconName={BsChevronCompactDown} />
+      </div>
     </StyledCard>
   );
 };
--- src/components/styles/HistoricalCard.styled.js
+++ src/components/styles/HistoricalCard.styled.js
@@ -1,19 +1,29 @@
 import styled from "styled-components";
 
 export const StyledCard = styled.div`
+  align-items: center;
   background-color: #fff;
   border: 2px solid ${({ borderColor }) => borderColor || "#c9c9c9c9"};
   border-radius: 6px;
   display: flex;
-  justify-content: space-between;
-
+  justify-content: center;
+  flex-direction: column;
   margin: 10px 0px;
   width: 85vw;
+`;
 
-  & > div {
-    padding: 20px 20px;
-    flex: 1;
-    text-align: center;
-    text-transform: capitalize;
-  }
+export const MinimizedContainer = styled.div`
+  padding: 20px 20px 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  text-transform: capitalize;
+  width: 100%;
+`;
+
+export const ExpandableContainer = styled(MinimizedContainer)`
+  height: ${({ expanded }) => (expanded ? "35px" : "0px")};
+  opacity: ${({ expanded }) => (expanded ? "1" : "0")};
+  padding: ${({ expanded }) => (expanded ? "20px 20px 10px 20px" : "0px")};
+  transition: all 0.3s ease;
 `;
