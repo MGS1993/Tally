c883ce64b892471d29082766fcb65cceee5bd60c
Manuel <m_guzman74@yahoo.com>
2022-04-03 23:48:42 -0500

    redesigned TallyDisplay with styled-components

--- src/components/TallyDisplay.js
+++ src/components/TallyDisplay.js
@@ -1,74 +1,30 @@
 import React from "react";
 
+import {
+  StyledDisplay,
+  TitleCard,
+  Wrapper,
+  ExpenseCard,
+} from "./styles/TallyDisplay.styled";
+
 const TallyDisplay = ({ data, currentUser, linkedUser }) => {
   //TODO add loading screen
   let balance =
     data?.calculatedUserExpenses - data?.calculatedOtherUserExpenses;
   return (
-    <div style={mainWrapper}>
-      <div style={cardStyle}>
-        <p style={text}>{`${currentUser}'s Expenses`}</p>
-        <div style={totalStyle}>{data?.calculatedUserExpenses} </div>
-      </div>
-
-      <div style={cardStyle}>
-        <p style={text}>Balance</p>
-        <div id="difference" style={balance < 0 ? DeficitText : totalStyle}>
-          {/* '|| 0' added as initial render value */}
-          {balance || 0}
-        </div>
-      </div>
-
-      <div style={cardStyle}>
-        <p style={text}>{`${linkedUser}'s Expenses`}</p>
-        <div style={totalStyle}>{data?.calculatedOtherUserExpenses}</div>
-      </div>
-    </div>
+    <StyledDisplay>
+      <Wrapper>
+        <TitleCard>{`${currentUser}'s Expenses`}</TitleCard>
+        <TitleCard>Balance</TitleCard>
+        <TitleCard>{`${linkedUser}'s Expenses`}</TitleCard>
+      </Wrapper>
+      <Wrapper>
+        <ExpenseCard>{data?.calculatedUserExpenses}</ExpenseCard>
+        <ExpenseCard deficit={balance < 0}>{balance || 0}</ExpenseCard>
+        <ExpenseCard>{data?.calculatedOtherUserExpenses}</ExpenseCard>
+      </Wrapper>
+    </StyledDisplay>
   );
-};
-
-const cardStyle = {
-  alignItems: "center",
-  display: "flex",
-  flex: "1",
-  flexDirection: "column",
-  margin: "10px",
-  textAlign: "center",
-  height: "100%",
-  justifyContent: "center",
-};
-
-const mainWrapper = {
-  alignItems: "center",
-  alignSelf: "center",
-  backgroundColor: "white",
-  borderRadius: "5px",
-  boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
-  display: "flex",
-  justifyContent: "center",
-  marginTop: "25px",
-  padding: "10px",
-  width: "80vw",
-};
-
-const text = {
-  color: "gray",
-  fontSize: "14px",
-  minHeight: "32px",
-  textTransform: "capitalize",
-};
-const DeficitText = {
-  color: "red",
-  fontSize: "28px",
-  fontWeight: "bold",
-  marginTop: "10%",
-};
-
-const totalStyle = {
-  color: "green",
-  fontSize: "28px",
-  fontWeight: "bold",
-  marginTop: "10%",
 };
 
 export default TallyDisplay;
---
+++ src/components/styles/TallyDisplay.styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const StyledDisplay = styled.div`
+  align-self: center;
+  background-color: dodgerblue;
+  border-radius: 10px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  width: 97vw;
+`;
+
+export const TitleCard = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 1;
+  font-size: 18px;
+  justify-content: center;
+  text-align: center;
+  text-transform: capitalize;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+export const ExpenseCard = styled(TitleCard)`
+  color: ${({ deficit }) => (deficit ? "#b82500" : "green")};
+  font-size: 26px;
+  font-weight: bold;
+  margin: 20px;
+`;
