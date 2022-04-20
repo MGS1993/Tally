0a99c0463d1af75485c4538b9ee80d732e347da1
Manuel <m_guzman74@yahoo.com>
2022-04-20 02:43:09 -0500

    changed Home css to styled-component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 
 import TallyDisplay from "../components/TallyDisplay";
 import TallyList from "../components/TallyList";
+import { StyledPage } from "../components/styles/Page.styled";
 
 const Home = ({ data, setData, currentUser, linkedUser }) => {
   //TODO fix rerender on opening nav menu
   // console.log(currentUser);
   // console.log(linkedUser);
   return (
-    <div style={mainWrapper}>
+    <StyledPage align="center" justifyContent="center" direction="column">
+      {/* <div style={mainWrapper}> */}
       <TallyDisplay
         data={data}
         currentUser={currentUser.userName}
@@ -16,14 +18,18 @@ const Home = ({ data, setData, currentUser, linkedUser }) => {
       />
 
       <TallyList data={data} userId={currentUser._id} setData={setData} />
-    </div>
+    </StyledPage>
+    // </div>
   );
 };
 
 const mainWrapper = {
+  // alignItems: "center",
   display: "flex",
   flexDirection: "column",
   flex: "1",
+  // overflow: "scroll",
+  // position: "relative",
 };
 
 export default Home;
